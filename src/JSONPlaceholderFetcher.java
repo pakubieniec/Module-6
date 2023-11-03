3dbb4a86211b7989b33a6ea563fe6198b4aac2f9
pakubieniec <pakubieniec@gmail.com>
2023-11-03 10:47:56 +0100

    Correct exercises 1 and 2 from Mod-6 and add excercise 3

--- src/JSONPlaceholderFetcher.java
+++ src/JSONPlaceholderFetcher.java
@@ -6,17 +6,15 @@ import java.net.http.HttpResponse;
 
 public class JSONPlaceholderFetcher {
 
-    private String id = "";
+    private String id;
     final HttpClient client = HttpClient.newBuilder().build();
-    URI url = URI.create("https://jsonplaceholder.typicode.com/posts/");
 
     public String getSinglePost(int id) {
-        url = URI.create("https://jsonplaceholder.typicode.com/posts/" + id);
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(url)
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts" + id))
                 .GET()
                 .build();
-        HttpResponse<String>  response = null;
+        HttpResponse<String> response;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (IOException e) {
@@ -27,12 +25,12 @@ public class JSONPlaceholderFetcher {
         return String.valueOf(response.body());
     }
 
-    public String getAllPosts(){
+    public String getAllPosts() {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(url)
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
                 .GET()
                 .build();
-        HttpResponse<String>  response = null;
+        HttpResponse<String> response;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (IOException e) {
@@ -42,20 +40,25 @@ public class JSONPlaceholderFetcher {
         }
         return String.valueOf(response.body());
     }
-    public boolean addPost(String post){
 
+    public boolean addPost(String post) {
+        HttpResponse<String> response;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
+                .POST(HttpRequest.BodyPublishers.ofString(post)
+                )
+                .build();
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return true;
     }
 }
 
-//public String getSinglePost(int id) -> używając metody GET, odpytaj o pojedynczy post,
-// używając podanego id przy budowie URI i zwróć ten post;
-//public String getAllPosts() -> używając metody GET odpytaj o wszystkie posty
-// i zwróć je;
-//public boolean addPost(String post) -> dodaj ciało do swojego zapytania: {
-       // title: 'foo',
-//body: 'bar',
-//userId: 1, }; a następnie używając metody POST dodaj nowe dane do istniejącego zasobu.
-// Zwróć true, kiedy operacja zakończy się sukcesem, false, jeśli nie.
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.io.IOException;
+import java.sql.SQLOutput;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         JSONPlaceholderFetcher json = new JSONPlaceholderFetcher();
-        System.out.println(json.getSinglePost(2));
-      // System.out.println(json.getAllPosts());
 
+        // System.out.println(json.getAllPosts());
+        System.out.println(json.addPost("{title: 'foo', body: 'bar', userId: 1}"));
+        System.out.println(json.getAllPosts());
 
     }
 }
